c18e60244e0e8bc2fef638e72cd1cf08ac37ac6c
frogfather <j.a.campbell@icloud.com>
2021-03-31 08:17:16 +0100

    add logging and handle exception on api

--- octopus.pas
+++ octopus.pas
@@ -130,7 +130,6 @@ begin
   userDir:=getUserDir;
 
   fileName:=userDir+'/.octopus.csv';
-  lbresults.items.add('filename set to '+filename);
   readSettings;
   getCurrentWeather;
 end;
@@ -310,6 +309,7 @@ begin
       MainTimer.Enabled:=(length(eOctopusApi.text) > 0) or (length(eOpenWeatherApi.Text)> 0);
       WeatherTimer.Interval:=(strToInt(epollInterval.text)*60000);
       WeatherTimer.Enabled:=ckWeather.Enabled;
+      lbresults.Items.add('['+formatdatetime('yyyy-mm-dd hh:nn:ss',now)+'] Settings read');
     except
       on e: Exception do messagedlg('','Error loading settings '+e.Message, mtError, [mbOK],'');
     end;
@@ -327,6 +327,7 @@ var
 begin
   try
     apiPollString:=formatDateTime('hh:nn:ss',tePoll.Time);
+    lbresults.Items.add('['+formatdatetime('yyyy-mm-dd hh:nn:ss',now)+'] Settings written');
   except
     apiPollString:=formatDateTime('hh:nn:ss',now);
   end;
@@ -360,7 +361,11 @@ begin
   HTTP.AddHeader('User-Agent', 'Mozilla/5.0 (compatible; fpweb)');
   HTTP.AddHeader('Content-Type', 'application/json');
   try
+    try
     result:=HTTP.Get(api);
+    except
+    on E : Exception do lbresults.Items.add('['+formatdatetime('yyyy-mm-dd hh:nn:ss',now)+'] error making api call '+E.Message);
+    end;
   finally
     http.free;
   end;
@@ -385,7 +390,7 @@ begin
     newTariff:=TTariff.create(jTariffItem);
     tariffs.AddEntity(newTariff);
     end;
-  lbresults.items.add('['+formatDateTime('yyyy-mm-dd hh:nn:ss', now)+'] Added '+inttostr(dm1.saveData(tariffs))+' records');
+  lbresults.items.add('['+formatDateTime('yyyy-mm-dd hh:nn:ss', now)+'] Added '+inttostr(dm1.saveData(tariffs))+' Octopus Agile records');
 end;
 
 procedure ToctopusForm.processOpenWeatherForecastData(data: TJSONObject);
@@ -406,7 +411,7 @@ begin
     newForecast:=TForecast.create(JForecastItem);
     forecasts.AddEntity(newForecast);
     end;
-  lbresults.items.add('['+formatDateTime('yyyy-mm-dd hh:nn:ss', now)+'] Added '+inttostr(dm1.saveData(forecasts))+' records');
+  lbresults.items.add('['+formatDateTime('yyyy-mm-dd hh:nn:ss', now)+'] Added '+inttostr(dm1.saveData(forecasts))+' weather forecast records');
 end;
 
 procedure ToctopusForm.processOpenWeatherCurrentData(data: TJSONObject);
